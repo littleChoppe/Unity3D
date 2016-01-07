1743485874d6ee1d36e9d12af7a948d55f071290
Little Choppe <piiy@163.com>
2016-01-07 23:47:01 +0800

    TheWarOfTanks
    control the tank to move and the camera

---
+++ TheWarOfTanks/Assets/script/camera/tankCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tankCamera : MonoBehaviour {
+
+	public Transform target;
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = target.position;
+	}
+}
---
+++ TheWarOfTanks/Assets/script/tank.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tank : MonoBehaviour {
+
+	public float moveSpeed;
+	public float rotateSpeed;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (Vector3.back * moveSpeed * Time.deltaTime);
+		}
+	
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (Vector3.down * rotateSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (Vector3.up * rotateSpeed * Time.deltaTime);
+		}
+	}
+}
