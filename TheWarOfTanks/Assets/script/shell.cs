68c1fe7d8a45ef0338a67d08819d11911fd0fbe1
Little Choppe <piiy@163.com>
2016-01-09 10:44:13 +0800

    The War Of Tank
    Make the tank can fire.

---
+++ TheWarOfTanks/Assets/script/shell.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shell : MonoBehaviour {
+	public GameObject explosionEffect;
+
+	void OnCollisionEnter()
+	{
+		Instantiate (explosionEffect, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}
---
+++ TheWarOfTanks/Assets/script/tankWeapon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tankWeapon : MonoBehaviour {
+
+	public GameObject shell;	//生成炮弹
+	public float shootPower;	//炮弹发射的力
+	public Transform shootPoint; //炮弹发射的位置
+
+	private AudioSource audioSorce;
+
+	void Start()
+	{
+		audioSorce = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//按下空格键发炮
+		if (Input.GetKeyDown (KeyCode.Space))
+			shoot ();
+
+	}
+
+	void shoot()
+	{
+		GameObject newShell = Instantiate (shell, shootPoint.position, shootPoint.rotation) as GameObject;
+		Rigidbody r = newShell.GetComponent<Rigidbody> ();
+		r.velocity = shootPoint.forward * shootPower;
+		audioSorce.Play ();
+	}
+}
